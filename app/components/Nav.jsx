72f760052525ad012902e14d16ec2ad53874c83d
Mayank Joshi <mayank@aisle.co>
2018-04-22 23:28:31 +0530

    style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,13 +2,39 @@ const React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
+    onSearch: function(e){
+        e.preventDefault();
+        alert("No function yet..");
+    },
     render: function() {
         return (
-            <div>
-                <h2>Navigation bar</h2>
-                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-                <Link to="/example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">Weather App</li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search Weather"/>
+                            </li>
+                            <li>
+                                <input type="submit" className="button"/>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
         );
     }
--- public/bundle.js
+++ public/bundle.js
@@ -35339,29 +35339,75 @@
 	var Nav = React.createClass({
 	    displayName: 'Nav',
 
+	    onSearch: function onSearch(e) {
+	        e.preventDefault();
+	        alert("No function yet..");
+	    },
 	    render: function render() {
 	        return React.createElement(
 	            'div',
-	            null,
+	            { className: 'top-bar' },
 	            React.createElement(
-	                'h2',
-	                null,
-	                'Navigation bar'
+	                'div',
+	                { className: 'top-bar-left' },
+	                React.createElement(
+	                    'ul',
+	                    { className: 'menu' },
+	                    React.createElement(
+	                        'li',
+	                        { className: 'menu-text' },
+	                        'Weather App'
+	                    ),
+	                    React.createElement(
+	                        'li',
+	                        null,
+	                        React.createElement(
+	                            IndexLink,
+	                            { to: '/', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	                            'Get Weather'
+	                        )
+	                    ),
+	                    React.createElement(
+	                        'li',
+	                        null,
+	                        React.createElement(
+	                            Link,
+	                            { to: '/about', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	                            'About'
+	                        )
+	                    ),
+	                    React.createElement(
+	                        'li',
+	                        null,
+	                        React.createElement(
+	                            Link,
+	                            { to: '/example', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
+	                            'Example'
+	                        )
+	                    )
+	                )
 	            ),
 	            React.createElement(
-	                IndexLink,
-	                { to: '/', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
-	                'Get Weather'
-	            ),
-	            React.createElement(
-	                Link,
-	                { to: '/about', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
-	                'About'
-	            ),
-	            React.createElement(
-	                Link,
-	                { to: '/example', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
-	                'Example'
+	                'div',
+	                { className: 'top-bar-right' },
+	                React.createElement(
+	                    'form',
+	                    { onSubmit: this.onSearch },
+	                    React.createElement(
+	                        'ul',
+	                        { className: 'menu' },
+	                        React.createElement(
+	                            'li',
+	                            null,
+	                            React.createElement('input', { type: 'search', placeholder: 'Search Weather' })
+	                        ),
+	                        React.createElement(
+	                            'li',
+	                            null,
+	                            React.createElement('input', { type: 'submit', className: 'button' })
+	                        )
+	                    )
+	                )
 	            )
 	        );
 	    }
